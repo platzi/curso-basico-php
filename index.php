73f6dd5cd8f51bfa414ebbdffabf0b030ffee51b
RetaxMaster <retaxmaster@hotmail.com>
2021-11-09 13:30:58 -0600

    Clase 08 - Parte 1

--- index.php
+++ index.php
@@ -1,33 +1,12 @@
 <?php
 
+// echo "Hola, mamá. Estoy programando con PHPCito \n";
+
 $nombre = "Carlos";
 $apellido = "Santana";
 
-// echo "No, no. Yo soy " . $nombre . " " . $apellido . "\n";
+// echo "No, no. Yo me llamo " . $nombre . " " . $apellido . "\n";
 
-// echo "No, no. Yo soy $nombre $apellido \n";
-// echo 'No, no. Yo soy $nombre $apellido \n';
+// echo "No, no. Yo me llamo $nombre $apellido \n";
 
-// echo "El resultado de 4 * 5 es " . (4 * 5);
-
-$edades = [
-    "Carlos" => 22,
-    "Mr. Michi" => 15,
-    "Juan" => 65
-];
-
-// var_dump( $edades );
-// print_r( $edades );
-
-// Esto ya es un comentario
-# Esto ya es otro comentario
-
-/* 
-
-Cualquieeeer cosa
-En multiples
-Líneas
-
-*/
-
-echo "\n";
+echo "El resultado de 4 x 5 es " . (4 * 5) . "\n";
