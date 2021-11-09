ac84fc8231eece26902260f4a690a65d8e9b06ba
RetaxMaster <retaxmaster@hotmail.com>
2021-11-09 12:18:34 -0600

    Clase 11 - Casting

---
+++ casting.php
@@ -0,0 +1,22 @@
+<?php
+
+/* $numerito = "5";
+
+var_dump( $numerito );
+
+$numerito = (int) $numerito;
+
+var_dump( $numerito ); */
+
+/* $dias = 5.89;
+$dias = (int) $dias;
+
+var_dump( $dias ); */
+
+$banderita = "";
+
+$banderita = (bool) $banderita;
+
+var_dump( $banderita );
+
+echo "\n";
