cd230712bf75582d8dc16d8b0d605ac326a6e306
RetaxMaster <retaxmaster@hotmail.com>
2021-11-09 11:01:01 -0600

    Clase 08 - Sintaxis básica

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
 <?php
 
-echo "Hola, Mundo";
+$nombre = "Carlos";
+$apellido = "Santana";
+
+// echo "No, no. Yo soy " . $nombre . " " . $apellido . "\n";
+
+// echo "No, no. Yo soy $nombre $apellido \n";
+// echo 'No, no. Yo soy $nombre $apellido \n';
+
+// echo "El resultado de 4 * 5 es " . (4 * 5);
+
+$edades = [
+    "Carlos" => 22,
+    "Mr. Michi" => 15,
+    "Juan" => 65
+];
+
+// var_dump( $edades );
+// print_r( $edades );
+
+// Esto ya es un comentario
+# Esto ya es otro comentario
+
+/* 
+
+Cualquieeeer cosa
+En multiples
+Líneas
+
+*/
+
+echo "\n";
