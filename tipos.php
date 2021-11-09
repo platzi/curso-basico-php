442bf503e489fa54ff8e242032fb9f4f0d499e24
RetaxMaster <retaxmaster@hotmail.com>
2021-11-09 11:57:29 -0600

    Clase 10 - Tipos

---
+++ tipos.php
@@ -0,0 +1,20 @@
+<?php
+
+/* $numerito = "23";
+
+$nuevo_numerito = $numerito + 2;
+
+var_dump($nuevo_numerito);
+var_dump($numerito); */
+
+/* $numerito = 10;
+$numerito = $numerito + 0.5;
+
+var_dump($numerito); */
+
+$papas = "10 papas en el costal";
+$cuantas_papas_hay = $papas + 5;
+
+echo $cuantas_papas_hay;
+
+echo "\n";
