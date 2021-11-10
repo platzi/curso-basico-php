926f11bed3c5ef6861c85123e309626073a9a4ea
RetaxMaster <retaxmaster@hotmail.com>
2021-11-10 10:53:43 -0600

    Clase 15

---
+++ operadores-aritmeticos.php
@@ -0,0 +1,16 @@
+<?php
+
+/* $resultado = 5 ** 3; // 5 x 5 x 5
+echo $resultado; */
+
+// echo (5 * 6) . " " . (80 / 4);
+
+/* echo 5 / 2 . "\n";
+echo 5 % 2; */
+
+$horas = 7501;
+
+echo "$horas segundo convertidos a horas son " . ($horas / 3600) . "\n";
+echo "Sobran " . ($horas % 3600) . " segundos.";
+
+echo "\n";
