884fd352a775e7261880bcf85ad3dba60c02d3d9
RetaxMaster <retaxmaster@hotmail.com>
2021-11-10 11:56:37 -0600

    Clase 18 - Otros operadores

---
+++ otros-operadores.php
@@ -0,0 +1,38 @@
+<?php
+
+// Operador de asignación
+/* $edad_de_jaimito = ($edad_de_pepito = 18) + 5;
+
+echo "La edad de pepito es $edad_de_pepito \n";
+echo "La edad de jaimito es $edad_de_jaimito"; */
+
+// Operadores de incremento
+
+$contador = 1;
+
+// $contador = $contador + 1;
+// $contador += 1;
+// $contador++;
+
+/* 
+
++=
+-= 
+*=
+/=
+
+*/
+
+/* $nombre = "Carlos";
+$nombre .=  " " . "Santana";
+
+echo $nombre; */
+
+$contador = 1;
+$resultado = $contador++;
+
+echo $contador . "\n";
+echo $resultado;
+
+
+echo "\n";
